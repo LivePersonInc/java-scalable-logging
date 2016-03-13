f902f4ad2ee534f3232080d3867489b3a4096aeb
tomer-ben-david <tomerbd1@gmail.com>
2016-03-13 08:45:40 +0200

    remove target 1.8

--- src/main/java/ScalableLogger.java
+++ src/main/java/ScalableLogger.java
@@ -1,21 +1,23 @@
 import org.slf4j.Logger;
 
 import java.util.concurrent.atomic.AtomicLong;
+import java.util.concurrent.atomic.LongAdder;
 
 public class ScalableLogger implements ScalableLogging {
-    private AtomicLong logCounter = new AtomicLong(0L);
+    private LongAdder logCounter = new LongAdder();
 
     public ThrottlingStatusEnum trace(Logger logger, String format, Object... arguments) {
         if (hasThrottlingConditionPassed(logCounter)) {
             logger.trace(format, arguments);
+            logCounter.increment();
             return ThrottlingStatusEnum.LOGGED;
         } else {
             return ThrottlingStatusEnum.THROTTLED;
         }
     }
 
-    public boolean hasThrottlingConditionPassed(final AtomicLong logCounter) {
-        if (logCounter.getAndIncrement() % 1000 == 0) {
+    public boolean hasThrottlingConditionPassed(final LongAdder logCounter) {
+        if (logCounter.longValue() % 1000 == 0) {
             return true;
         } else {
             return false;
