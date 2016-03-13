9570d7fb8ceadbf9fc759d4a4959ddecac95431e
tomer-ben-david <tomerbd1@gmail.com>
2016-03-13 11:21:20 +0200

    fix compilation error

--- src/test/java/ScalableLoggerTest.java
+++ src/test/java/ScalableLoggerTest.java
@@ -2,10 +2,17 @@ import org.junit.Assert;
 import org.junit.Test;
 
 import java.util.concurrent.atomic.AtomicLong;
+import java.util.concurrent.atomic.LongAdder;
 
 public class ScalableLoggerTest {
     @Test public void hasThrottlingConditionPassed() throws Exception {
-        Assert.assertFalse(new ScalableLogger().hasThrottlingConditionPassed(new AtomicLong(543)));
-        Assert.assertTrue(new ScalableLogger().hasThrottlingConditionPassed(new AtomicLong(1000)));
+        LongAdder long434 = new LongAdder();
+        long434.add(434);
+
+        LongAdder long1000 = new LongAdder();
+        long1000.add(1000);
+
+        Assert.assertFalse(new ScalableLogger().hasThrottlingConditionPassed(long434));
+        Assert.assertTrue(new ScalableLogger().hasThrottlingConditionPassed(long1000));
     }
 }
